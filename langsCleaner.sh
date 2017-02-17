2c0f149ae063687a0cc4a23f208fa6c00c785607
ummeegge <ummeegge@web.de>
2017-02-17 15:49:41 +0100

    Displays also line count difference

--- langsCleaner.sh
+++ langsCleaner.sh
@@ -12,20 +12,22 @@
 # generate all diffs by copying the new files to the building env which should
 # then delivers applicable diffs.
 #
-# $Author: ummeegge ; $date: 07.02.2017
+# $Author: ummeegge at web de ; $date: 17.02.2017
 ############################################################################################
 
 # End of line is seperator
 IFS=$'\n'
 
 # Locations
-DIR="/tmp/strings_not_found";
+DIR="/tmp/langs_strings_not_found";
 LANGS="/var/ipfire/langs";
 CGI="/srv/web/ipfire/cgi-bin"
 MENU="/var/ipfire/menu.d";
 PLS="/var/ipfire";
 THEMES="/srv/web/ipfire/html/themes/*/*";
 PAKFIRE="/opt/pakfire/lib";
+LCB="LineCounter_before";
+LCA="LineCounter_after";
 
 # Formatting functions
 COLUMNS="$(tput cols)";
@@ -35,18 +37,20 @@ B=$(tput setaf 6);
 b=$(tput bold);
 N=$(tput sgr0);
 # Text
-TITEL="Searcher and clean up script for dead language file strings - You can easily go for a tankard of coffe now ;-)";
-END="That´s it all work has been done, will go for a beer now. Tschüssle";
+TITEL="Clean up script for dead language file strings - You can easily go for a tankard of coffe now ;-)";
+END="That´s it, all work has been done, will go for a beer now. Tschüssle";
 HASSLEA="Puhh lots of hassle here today :-| ...";
 HASSLE="Need to strip the lost ones out... stay tuned ;-) ";
 ERROR="If there are some 'sed: -e expression #1, char 1: unknown command' errors, ";
 ERRORA="there might be variables with an slash '/' in it .";
-ERRORB="A test will now follow, so if the script has been finished, it will print the strings out for you.";
-ERRORC="You can also find all files under ${DIR} .";
+ERRORB="A test will now follow, so if the script has been finished, it will print the strings out for you if something exists.";
+ERRORC="You can find all files under ${DIR} .";
 COPY="All languages has been compared and the lost strings has been printed to ${DIR} ."
 LANG="The language cache has been updated. From now on, all changes can be overviewed over the WUI. ";
 RESULT="Show you all entries which has not been deleted, please delete them manually or try a better script.";
-REUSLTA="All files can be found under ${DIR} .";
+RESULTA="All files can be found under ${DIR} if something has been done there.";
+RESULTB="This count of deleted lines can also be found under '${DIR}/deleted_lines_result' if there has been something deleted.";
+RESULTC="Count of all deleted lines per language";
 # Seperator functions
 seperator(){ printf -v _hr "%*s" ${COLUMNS} && echo ${_hr// /${1-=}}; }
 
@@ -60,15 +64,21 @@ seperator;
 echo;
 echo;
 
-# Status bar
-while true; do echo -n .; sleep 1; done &
-trap 'kill $!' SIGTERM SIGKILL
-
 # Add dir if not already presant and change working place
 if [ ! -d "${DIR}" ]; then
     mkdir ${DIR};
 fi
 cd ${DIR};
+
+# Backup of existing files
+cp -R ${LANGS}/ ${DIR}/langs_orig_bck
+
+# Count lines before processing
+wc -l /var/ipfire/langs/*.pl | tail -n +2 >> ${DIR}/${LCB};
+
+# Status bar
+while true; do echo -n .; sleep 1; done &
+trap 'kill $!' SIGTERM SIGKILL
 
 # Investigate unused entries
 
@@ -258,7 +268,7 @@ do
     sed -i "/${l}/d" ${LANGS}/tr.pl;
 done
 
-echo -e "${B}Turkish is done... ${N}";
+echo -e "${B}Turkish is done${N}";
 echo;
 
 update-lang-cache > /dev/null 2>&1
@@ -285,8 +295,6 @@ do
     fi
 done
 
-echo;
-echo;
 echo -e "${B}German has been tested... ${N}";
 echo;
 echo -e "${R}8 languages are left${N}";
@@ -383,8 +391,11 @@ do
     fi
 done
 
-echo "${B}Turkish has been tested... ${N}";
+echo "${B}Turkish has been tested${N}";
 
+kill $!
+
+clear;
 echo;
 echo;
 seperator;
@@ -393,13 +404,27 @@ printf "%*s\n" $(((${#RESULTA}+$COLUMNS)/2)) "${RESULTA}";
 seperator;
 echo;
 echo;
-head -n99999999 ${DIR}/*_rest_entries;
+head -n99999999 ${DIR}/*_rest_entries > /dev/null 2>&1;
+echo;
+echo;
+
+# Numbers of deletion
+seperator;
+printf "%*s\n" $(((${#RESULTC}+$COLUMNS)/2)) "${RESULTC}";
+seperator;
+echo;
+echo;
+# Count lines after processing
+wc -l /var/ipfire/langs/*.pl | tail -n +2 >> ${DIR}/${LCA};
+# Processing number of deletion
+paste {$DIR/$LCB,$DIR/$LCA} | awk '{ print $1-$3,"\tLines has been deleted in     "$2 }' > ${DIR}/deleted_lines_result;
+cat ${DIR}/deleted_lines_result;
+echo;
+printf "%*s\n" $(((${#RESULTB}+$COLUMNS)/2)) "${RESULTB}";
 echo;
 echo;
 printf "%*s\n" $(((${#END}+$COLUMNS)/2)) "${END}";
 echo;
 echo;
-
-kill $!
 
 # End script
